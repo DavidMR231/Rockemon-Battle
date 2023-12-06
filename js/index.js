c8dc9d8899d44237a80cc96eda0b09dfc2290efb
DAVIDMR <morenorojasdavidsantiago@gmail.com>
2023-12-06 01:31:41 -0500

    Mejora

--- js/index.js
+++ js/index.js
@@ -1,7 +1,15 @@
-function seleccionarmascotaJugador() {
-  
+function iniciarjuego() {
+    let btnSeleccionarMascota = document.getElementById("btn-seleccionar");
+    btnSeleccionarMascota.addEventListener("click", seleccionarMascotaJugador);
 }
-let btnMascotaJugador = document.getElementById("btn-mascota");
-  btnMascotaJugador.addEventListener("click", seleccionarmascotaJugador);
-let btnReiniciar = document.getElementById("btn-reiniciar");
-let btnvolverJugar = document.getElementById("btn-volver-jugar");
+
+function seleccionarMascotaJugador() {
+    let seleccion = document.getElementById("mascotas");
+    let mascotaSeleccionada = seleccion.options[seleccion.selectedIndex].text;
+    alert("Usted ha seleccionado " + mascotaSeleccionada);
+}
+
+let btnReiniciar = document.getElementById("btn-volver-jugar");
+let btnTerminar = document.getElementById("btn-terminar");
+
+document.addEventListener("DOMContentLoaded", iniciarjuego);
