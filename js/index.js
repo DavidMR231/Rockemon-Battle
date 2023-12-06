bdf64b9908807ec259ba04b951529719ba82ba97
DAVIDMR <morenorojasdavidsantiago@gmail.com>
2023-12-06 08:41:29 -0500

    Elegir mascotas

--- js/index.js
+++ js/index.js
@@ -8,6 +8,19 @@ function seleccionarMascotaJugador() {
   let seleccion = document.getElementById("mascotas");
   let mascotaSeleccionada = seleccion.options[seleccion.selectedIndex].text;
   spanMascotaJugador.innerHTML = mascotaSeleccionada;
+  seleccionarMascotaEnemigo()
+}
+function seleccionarMascotaEnemigo() {
+  let ataquealeatorio = aleatorio(1,8)
+  let seleccion = document.getElementById("mascotas");
+  let opcionAleatoria = seleccion.options[ataquealeatorio - 1];
+  let spanMascotaEnemigo = document.getElementById("mascota-enemigo");
+  spanMascotaEnemigo.textContent = opcionAleatoria.text;
+}
+
+// Función para generar un número aleatorio en un rango dado
+function aleatorio(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 let btnReiniciar = document.getElementById("btn-volver-jugar");
