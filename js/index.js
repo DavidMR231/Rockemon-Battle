e022c90a53042d6cb5dfe06707d040a5aac8c853
DAVIDMR <morenorojasdavidsantiago@gmail.com>
2023-12-06 00:50:06 -0500

    Mejora del juego

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function seleccionarmascotaJugador() {
+  
+}
+let btnMascotaJugador = document.getElementById("btn-mascota");
+  btnMascotaJugador.addEventListener("click", seleccionarmascotaJugador);
+let btnReiniciar = document.getElementById("btn-reiniciar");
+let btnvolverJugar = document.getElementById("btn-volver-jugar");
