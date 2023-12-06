916cad6e5dc252145b4c414a5f14416f4d8c4e21
DAVIDMR <morenorojasdavidsantiago@gmail.com>
2023-12-06 08:57:08 -0500

    Mejora

--- js/index.js
+++ js/index.js
@@ -1,24 +1,37 @@
-function iniciarjuego() {
+let ataquejugador = function iniciarjuego() {
   let btnSeleccionarMascota = document.getElementById("btn-seleccionar");
   btnSeleccionarMascota.addEventListener("click", seleccionarMascotaJugador);
-}
+  let btnFuego = document.getElementById("btn-fuego");
+  btnFuego.addEventListener("click", ataqueFuego);
+  let btnAgua = document.getElementById("btn-agua");
+  btnAgua.addEventListener("click", ataqueAgua);
+  let btnTierra = document.getElementById("btn-tierra");
+  btnTierra.addEventListener("click", atagueTierra);
+};
 
 function seleccionarMascotaJugador() {
   let spanMascotaJugador = document.getElementById("mascota-jugador");
   let seleccion = document.getElementById("mascotas");
   let mascotaSeleccionada = seleccion.options[seleccion.selectedIndex].text;
   spanMascotaJugador.innerHTML = mascotaSeleccionada;
-  seleccionarMascotaEnemigo()
+  seleccionarMascotaEnemigo();
 }
 function seleccionarMascotaEnemigo() {
-  let ataquealeatorio = aleatorio(1,8)
+  let ataquealeatorio = aleatorio(1, 8);
   let seleccion = document.getElementById("mascotas");
   let opcionAleatoria = seleccion.options[ataquealeatorio - 1];
   let spanMascotaEnemigo = document.getElementById("mascota-enemigo");
   spanMascotaEnemigo.textContent = opcionAleatoria.text;
 }
-
-// Función para generar un número aleatorio en un rango dado
+function ataqueFuego() {
+  ataquejugador = "Fuego";
+}
+function ataqueAgua() {
+  ataquejugador = "Agua";
+}
+function ataqueTierra() {
+  ataquejugador = "Tierra";
+}
 function aleatorio(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
